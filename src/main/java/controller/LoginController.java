8b27702172509d76f6b48fd18cfd65e7b17416f9
mamjow <mj.moshiri@gmail.com>
2021-01-06 17:11:01 +0100

    login GUI

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+    public TextField loginUsername;
+    public TextField loginUnMaskedPassword;
+    public PasswordField loginMaskedPassword;
+    public Button passShowBtn;
+    public Button loginbtn;
+    public Button cancelBtn;
+
+    public void showPassword(MouseEvent mouseEvent) {
+    }
+
+    public void unShowPassword(MouseEvent mouseEvent) {
+    }
+
+    public void userLogin(ActionEvent actionEvent) {
+    }
+
+    public void loginCancel(ActionEvent actionEvent) {
+    }
+}
