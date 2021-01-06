e7a845cc15217cc5962fd21440b99e97187d34e3
CyberWorksIzzy <i.bencherif@student.fontys.nl>
2021-01-06 14:19:29 +0100

    studentSignInOut fxml and StudentDAO

--- src/main/java/controller/studentSignInOutController.java
+++ src/main/java/controller/studentSignInOutController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class studentSignInOutController {
+    public Button addCourse;
+    public Button removeCourse;
+    public ListView courseListAdd;
+    public ListView courseListRemove;
+
+    public void addCoursesToList(ActionEvent actionEvent) {
+    }
+
+    public void removeCoursesFromList(ActionEvent actionEvent) {
+    }
 }
