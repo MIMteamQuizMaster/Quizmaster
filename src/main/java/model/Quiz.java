967000330f05d0021c5bff311bbeae60fd82310b
mamjow <mj.moshiri@gmail.com>
2021-01-06 13:09:25 +0100

    question af

--- src/main/java/model/Quiz.java
+++ src/main/java/model/Quiz.java
@@ -1,3 +1,19 @@
 package model;
 
-public class Quiz {}
+import java.util.List;
+
+public class Quiz {
+    private List<Question> questions;
+    private double succsesDefinition;
+
+    public Quiz(double succsesDefinition) {
+        this.succsesDefinition = succsesDefinition;
+    }
+    public void addQuestion(Question q){
+        questions.add(q);
+    }
+    public Question showQuestion(int i){
+        return questions.get(i);
+    }
+
+}
