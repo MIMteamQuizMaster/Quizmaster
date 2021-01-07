eade1fb6de42d02e2f8fc4d1af843771e79c0b75
CyberWorksIzzy <i.bencherif@student.fontys.nl>
2021-01-07 02:46:47 +0100

    question fxml en start QuestionDAO extend

--- src/main/java/controller/CreateUpdateQuestionController.java
+++ src/main/java/controller/CreateUpdateQuestionController.java
@@ -1,17 +1,33 @@
 package controller;
 
+import database.mysql.DBAccess;
+import database.mysql.QuestionDAO;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import model.Question;
 
 public class CreateUpdateQuestionController {
-    public TextArea question;
-    public TextArea correctAnswer;
-    public TextArea wrongAnswer1;
-    public TextArea wrongAnswer2;
-    public TextArea wrongAnswer3;
-    public Button saveQuestion;
-    public Button cancelQuestion;
+
+    private QuestionDAO questionDAO;
+    private DBAccess dBaccess;
+    private Question question;
+
+    @FXML
+    private TextArea questionText;
+    @FXML
+    private TextArea correctAnswer;
+    @FXML
+    private TextArea wrongAnswer1;
+    @FXML
+    private TextArea wrongAnswer2;
+    @FXML
+    private TextArea wrongAnswer3;
+    @FXML
+    private Button saveQuestion;
+    @FXML
+    private Button cancelQuestion;
 
     public void saveQuestionAction(ActionEvent actionEvent) {
     }
