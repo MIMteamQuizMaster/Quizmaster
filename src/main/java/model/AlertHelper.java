ff80bd5ea9ea3af0236602c1aaa301fc1b8ba194
mamjow <mj.moshiri@gmail.com>
2021-01-10 18:24:46 +0100

    add athentication to users done. add users done, update users. done should add delete user ? and work on mutli rol users and update role

---
+++ src/main/java/model/AlertHelper.java
@@ -0,0 +1,25 @@
+package model;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.stage.StageStyle;
+
+import java.util.Optional;
+
+public class AlertHelper {
+    public static void dialog(Alert.AlertType alertType, String statement) {
+        Alert alert = new Alert(alertType, statement);
+        alert.initStyle(StageStyle.UTILITY);
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
+
+    public static boolean confirmationDialog(String text){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Weet je zeker dat?");
+        alert.setContentText(text);
+        Optional<ButtonType> option = alert.showAndWait();
+        return ButtonType.OK.equals(option.get());
+    }
+}
--- src/main/java/model/TechnicalAdministrator.java
+++ src/main/java/model/TechnicalAdministrator.java
@@ -6,7 +6,6 @@ public class TechnicalAdministrator extends User {
 
     public TechnicalAdministrator(int userId, String firstname, String lastname) {
         super(userId, firstname, lastname);
-//        super.addRole(Role.TECHNICAL_ADMINISTRATOR);
     }
 
 }
