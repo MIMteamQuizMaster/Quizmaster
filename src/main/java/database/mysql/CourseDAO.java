94191165c68b90f64eeb03bf2d42f31a3c65281f
max <maxaldenm@gmail.com>
2021-01-12 16:38:28 +0100

    Create CourseDAO.java: Add storeCourse(Course mpCourse): inserts a course into the DB. Add getCourseById(int course_id) UNFINISHED: gets a course from a given course id. requires missing dependency CoordinatorDAO.

--- src/main/java/database/mysql/CourseDAO.java
+++ src/main/java/database/mysql/CourseDAO.java
@@ -1,5 +1,114 @@
 package database.mysql;
 
-public class CourseDAO {
-    // commit test
+import com.mysql.cj.xdevapi.Result;
+import model.Answer;
+import model.Course;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseDAO extends AbstractDAO {
+
+    public CourseDAO(DBAccess dBaccess) {
+        super(dBaccess);
+    }
+
+    public Course getCourseById(int course_id) {
+        String sql = "SELECT name, coordinator_id FROM course WHERE id = " + course_id;
+
+        try {
+            Course mpCourse;
+            PreparedStatement preparedStatement = getStatement(sql);
+            ResultSet rs = executeSelectPreparedStatement(preparedStatement);
+
+            // need coordinator to create new user
+
+//            String answerToQuestion = rs.getString(4);
+//            int correct = rs.getInt(3);
+//            boolean trueFalse = false;
+//            if (correct == 1)
+//            {
+//                trueFalse = true;
+//            }
+//            answer = new Answer(trueFalse, answerToQuestion);
+//            answer.setId(rs.getInt(1));
+//            answer.setQuestionId(rs.getInt(2));
+//            answers.add(answer);
+
+        } catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
+        }
+
+
+        return null;
+    }
+
+    public void storeCourse(Course mpCourse) {
+        String sql = "INSERT INTO course(coordinator_user_id, name, startDate, endDate)" + "VALUES (?,?,?,?)";
+        try {
+            PreparedStatement preparedStatement = getStatement(sql);
+            preparedStatement.setInt(1, mpCourse.getCoordinator().getUserId());
+            preparedStatement.setString(2, mpCourse.getName());
+            preparedStatement.setString(3, mpCourse.getStartDate());
+            preparedStatement.setString(4, mpCourse.getEndDate());
+            executeManipulatePreparedStatement(preparedStatement);
+        } catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
+        }
+    }
+
+
+
+
+//    public List<Hond> getHondByKlant(mpKlant) {
+//        String sql = "Select k.klant from Hond h, Klant k where k.klantnr = h.klantnr";
+//
+//        List<Hond> result = new ArrayList<>();
+//        try {
+//            setupPreparedStatement(sql);
+//            ResultSet rs = executeSelectStatement();
+//
+//            while(rs.next()) {
+//                int chipnr = rs.getInt("chipnr");
+//                String hondnaam = rs.getString("hondnaam");
+//                String ras = rs.getString("ras");
+//                int klantnr = rs.getInt("klantnr");
+//                String telefoon = rs.getString("telefoon");
+//                Klant resultElement = new Klant(klantnr, voorletters, voorvoegsels, achternaam, telefoon);
+//                result.add(resultElement);
+//            }
+//
+//
+//        } catch (SQLException e) {
+//            System.out.println("SQL error " + e.getMessage());
+//        }
+//        return result;
+//    }
+//
+//    public Klant getKlantById (int klantnummer) {
+//        String sql = "SELECT * FROM Klant WHERE klantnr = ?";
+//        Klant result = null;
+//        try {
+//            setupPreparedStatement(sql);
+//            preparedStatement.setInt(1, klantnummer);
+//            ResultSet rs = executeSelectStatement();
+//
+//            while (rs.next()) {
+//
+//                int klantnr = rs.getInt("klantnr");
+//                String voorletters = rs.getString("voorletters");
+//                String voorvoegsel = rs.getString("voorvoegsels");
+//                String achternaam = rs.getString("achternaam");
+//                String telefoon = rs.getString("telefoon");
+//
+//                result = new Klant(klantnr, voorletters, voorvoegsel, achternaam, telefoon);
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("SQL error " + e.getMessage());
+//        }
+//        return result;
+//    }
+//    // commit test
 }
--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -6,6 +6,10 @@ public class Course {
     private int dbId;
     private String name;
     private Coordinator coordinator;
+
+
+    private String startDate;
+    private String endDate;
     private List<Quiz> quizzes;
     private List<Group> groups;
 
@@ -14,16 +18,16 @@ public class Course {
         this.coordinator = coordinator;
     }
 
-    public boolean addQuiz(Coordinator coordinator,Quiz quiz){
-        if (coordinator == this.coordinator){
+    public boolean addQuiz(Coordinator coordinator, Quiz quiz) {
+        if (coordinator == this.coordinator) {
             quizzes.add(quiz);
             return true;
         }
         return false;
     }
 
-    public boolean addGroup(Coordinator coordinator,Group group){
-        if (coordinator == this.coordinator){
+    public boolean addGroup(Coordinator coordinator, Group group) {
+        if (coordinator == this.coordinator) {
             groups.add(group);
             return true;
         }
@@ -60,5 +64,21 @@ public class Course {
 
     public void setDbId(int dbId) {
         this.dbId = dbId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
     }
 }
